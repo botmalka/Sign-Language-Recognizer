00a16cf6b4e555442fcf7ad149e7efe5e6096501
Tori <tori.shanholtz@outlook.com>
2022-04-13 10:44:19 -0700

    updated dataset to remove numbers

--- CameraReader.py
+++ CameraReader.py
@@ -17,7 +17,7 @@ X = df[features]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20) #random_state=42
 
 #defines model
-model = KNeighborsClassifier(n_neighbors=36)
+model = KNeighborsClassifier(n_neighbors=26)
 model.fit(X_train, y_train)
 model.predict(X_test)
 
